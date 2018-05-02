0f1ba594be64fc9d833000858c9899da9f149ad3
John Mooney <jrmint46@gmail.com>
2018-05-01 22:00:43 -0400

    Added methods to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,20 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		user = User.find_by(name: params[:user][:name])
+		user = user.try(:authenticate, params[:user][:password])
 
+		if user
+			session[:user_id] = user.id
+			@user = user
+		else
+			redirect_to login_path
+		end
 	end
 
 	def destroy
+		session.delete :user_id
 
+		redirect_to login_path
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
 	def new
-
+		@user = User.new
 	end
 
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			redirect_to movies_path
 		else
 			render :new
@@ -15,6 +16,6 @@ class UsersController < ApplicationController
 	private
 
 	def user_params
-		params.require(:user).permit(:name, :password, :password_confirmation)
+		params.require(:user).permit(:username, :email, :password, :password_confirmation)
 	end
 end
