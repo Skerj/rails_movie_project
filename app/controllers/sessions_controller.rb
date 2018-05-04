28daa9b1b1422097ade3ccd5a5431cdfdea72f6e
John Mooney <jrmint46@gmail.com>
2018-05-04 06:09:45 -0400

    Edited sessions controller create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,11 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by(name: params[:user][:name])
-		user = user.try(:authenticate, params[:user][:password])
+		#raise params.inspect
+		user = User.find_by(username: params[:user][:username])
+		session[:user_id] = user.id
 
-		if user
-			session[:user_id] = user.id
-			@user = user
-		else
-			redirect_to login_path
-		end
+		redirect_to root_path
 	end
 
 	def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
 	get '/login' => 'sessions#new'
 	post '/login' => 'sessions#create'
 	post '/logout' => 'sessions#destroy'
-	
+
   resources :movies
   resources :actors
   resources :users, only: [:new, :create]
+
+  root 'welcome#home'
 end
