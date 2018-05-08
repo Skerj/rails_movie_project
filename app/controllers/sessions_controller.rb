373edd3223c9d98d0d1331d4cca0ff0a883d56bd
John Mooney <jrmint46@gmail.com>
2018-05-08 18:33:51 -0400

    Added omniauth conditionals for sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,25 @@
 class SessionsController < ApplicationController
 	def new
-		@user = User.new
+		if logged_in?
+			redirect_to root_path
+		else
+			@user = User.new
+		end
 	end
 
 	def create
 		#raise request.env["omniauth.auth"].inspect
 		if auth_hash = request.env["omniauth.auth"]
-
+			#raise auth_hash.inspect
+			oauth_email = request.env["omniauth.auth"]["email"]
+			if user = User.find_by(:email => oauth_email)
+				session[:user_id] = user.id
+				redirect_to root_path
+			else
+				user = User.create(email: oauth_email)
+				session[:user_id] = user.id
+				redirect_to root_path
+			end
 		else
 			user = User.find_by(username: params[:user][:username])
 			if user && user.authenticate(params[:user][:password])
