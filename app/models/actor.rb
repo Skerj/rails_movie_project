c6f1d2609faaff54ea3f7492341c2ffc81aca7de
John Mooney <jrmint46@gmail.com>
2018-05-08 23:05:43 -0400

    Added actor model scope methods

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,4 +3,12 @@ class Actor < ApplicationRecord
 	has_many :movies, through: :actor_movies
 	validates :name, presence: true
 	validates :age, presence: true
+
+	def self.oldest_actors
+		self.order(:age).last(3)
+	end
+
+	def self.youngest_actors
+		self.order(:age).first(3)
+	end
 end
