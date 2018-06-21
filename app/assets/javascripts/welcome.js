a856387b2c9c34ee490f8fb6982c7ef16b45cd26
John Mooney <jrmint46@gmail.com>
2018-06-21 18:54:06 -0400

    Made a comment model

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,7 +1,48 @@
-debugger
 $(function() {
-	$('.movies_link').on("click", function(event) {
-	console.log("hey");
-	event.preventDefault();
+	$('.movies_link').on("click", function(e) {
+		// debugger
+		$.ajax({
+			method: "GET",
+			url: this.href
+		}).success(function(response) {
+			$('div.movie_info').html(response);
+		});
+	e.preventDefault();
 	});
 })
+
+//     // Fire some ajax.
+//     // $.ajax({
+//     //   method: "GET",
+//     //   url: this.href
+//     // }).success(function(response){
+//     //   // Get the response (it's the variable data)
+//     //   $("div.comments").html(response)
+//     //   // We'd really want to load that data into the DOM (add it to the current page)
+//     // }).error(function(notNeeded){
+//     //   alert("we broke!!!!")
+//     // });
+// ------------------------
+// Submit Comments via AJAX - Soon to be replaced by remote true
+// $(function(){
+//   $("#new_comment").on("submit", function(e){
+//     // 1. we need the URL to submit the POST request too
+//     // 2. we need the form data.
+//
+//     // Low level
+//     $.ajax({
+//       type: ($("input[name='_method']").val() || this.method),
+//       url: this.action,
+//       data: $(this).serialize();, // either JSON or querystring serializing
+//       success: function(response){
+//         $("#comment_content").val("");
+//         var $ol = $("div.comments ol")
+//         $ol.append(response);
+//       }
+//     });
+//
+//     // Send a POST request to the correct place that form would've gone too anyway
+//     // along with the actual form data.
+//     e.preventDefault();
+//   })
+// });
---
+++ app/models/comment.rb
@@ -0,0 +1,5 @@
+class Comment < ApplicationRecord
+	belongs_to :movie
+	belongs_to :actor
+	belongs_to :user
+end
