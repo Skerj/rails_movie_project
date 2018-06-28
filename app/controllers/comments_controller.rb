93156c2cb53ff65ab5c0ed56b7398c2fa8820021
John Mooney <jrmint46@gmail.com>
2018-06-28 14:55:40 -0400

    Added comments controller functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,44 +2,16 @@ class CommentsController < ApplicationController
 	before_action :authentication_required
 	before_action :user_info
 
-	def index
-
-	end
-
-	def show
-		@comment = Comment.find(params[:id])
-	end
-
-	def new
-		#raise params.inspect
-		@comment = Comment.new
-
-	end
-
 	def create
-		@comment = Comment.new(comment_params)
-		if @comment.save
-			redirect_to comment_path(@comment)
-		else
-			render :new
-		end
-	end
-
-	def edit
-
-	end
-
-	def update
-
-	end
-
-	def destroy
-
+		@comment = @commentable.comments.new comment_params
+		@comment.user = user_info
+		@comment.save
+		redirect_to @commentable
 	end
 
 	private
 
 	def comment_params
-		params.require(:comment).permit(:movie_ids, :actor_ids, :user_ids, :body, :commentable_ids, :commentable_type)
+		params.require(:comment).permit(:body)
 	end
 end
