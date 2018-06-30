46680a9a49c2ba2b7ac09690c235b4f6e05bc22c
John Mooney <jrmint46@gmail.com>
2018-06-29 23:26:45 -0400

    Added pg and rails_12factor gems for heroku

---
+++ app/assets/javascripts/comments.js
@@ -0,0 +1,27 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+// Submit Comments via AJAX - Soon to be replaced by remote true
+// $(function(){
+//   $("#new_comment").on("submit", function(e){
+//     // 1. we need the URL to submit the POST request too
+//     // 2. we need the form data.
+//
+//     // Low level
+//     $.ajax({
+//       type: ($("input[name='_method']").val() || this.method),
+//       url: this.action,
+//       data: $(this).serialize();, // either JSON or querystring serializing
+//       success: function(response){
+//         $("#comment_content").val("");
+//         var $ol = $("div.comments ol")
+//         $ol.append(response);
+//       }
+//     });
+//
+//     // Send a POST request to the correct place that form would've gone too anyway
+//     // along with the actual form data.
+//     e.preventDefault();
+//   })
+// });
+
