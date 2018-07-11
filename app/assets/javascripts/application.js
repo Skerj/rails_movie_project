bff312686f221d623605fa3dd9ac4e5e32b7f80b
John Mooney <jrmint46@gmail.com>
2018-07-11 00:33:37 -0400

    Changed order of files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require rails-ujs
 //= require turbolinks
 //= require popper
-//- require bootstrap
 //= require_tree .
 //= require welcome
 
